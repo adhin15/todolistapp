1d6598af9815038011745795825cd9494d913282
adhin15 <adhin5643@gmail.com>
2021-11-13 10:40:15 +0700

    Cleaning App.js so there's no logic or layouting

--- src/App.js
+++ src/App.js
@@ -1,43 +1,17 @@
-import { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Form from './components/Form';
-import TodoList from "./components/TodoList";
+
 
 function App() {
-  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
-  const [input, setInput] = useState("");
-  const [todos, setTodos] = useState(initialState);
-  const [editTodo, setEditTodo] = useState(null);
-  useEffect(() => {
-    localStorage.setItem("todos", JSON.stringify(todos));
-  }, [todos]);
+
+
   return (
     <div className="App">
-      <div className="container-fluid p-5 align-midle">
-        <h1> To-Do List App </h1>
-        <div className="row justify-content-center">
-          <div className="col-md-4">
-            <div
-              className="container mt-5 p-4 rounded shadow-lg app-container"
-              style={{ background: "#0d2b34" }}>
+      <Form />
+    </div>
 
-              <Form
-                input={input}
-                setInput={setInput}
-                todos={todos}
-                setTodos={setTodos}
-                editTodo={editTodo}
-                setEditTodo={setEditTodo}
-              />
-              <TodoList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo} />
-            </div>
-          </div>
-        </div>
-
-      </div>
-
-    </div >)
-    ;
+  );
 }
 
 export default App;
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,7 +1,16 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
+import TodoList from "./TodoList";
 
-const Form = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
+const Form = () => {
+  const initialState = JSON.parse(localStorage.getItem("todos")) || [];
+  const [input, setInput] = useState("");
+  const [todos, setTodos] = useState(initialState);
+  const [editTodo, setEditTodo] = useState(null);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
+
   const updateTodo = (title, id, completed) => {
     const newTodo = todos.map((todo) => (todo.id === id ? { title, id, completed } : todo));
     setTodos(newTodo);
@@ -31,25 +40,41 @@ const Form = ({ input, setInput, todos, setTodos, editTodo, setEditTodo }) => {
 
   return (
     <div>
-      <form onSubmit={onFomSubmit}>
-        <div className="row">
-          <div className="col-md-10">
-            <input
-              type="text"
-              className="form-control input-text"
-              placeholder="Enter A Task"
-              value={input}
-              required
-              onChange={onInputChange}
-            />
+      <div className="container-fluid p-5 align-midle">
+        <h1> To-Do List App </h1>
+        <div className="row justify-content-center">
+          <div className="col-md-4">
+            <div
+              className="container mt-5 p-4 rounded shadow-lg app-container"
+              style={{ background: "#0d2b34" }}>
+
+              <form onSubmit={onFomSubmit}>
+                <div className="row">
+                  <div className="col-md-10">
+                    <input
+                      type="text"
+                      className="form-control input-text"
+                      placeholder="Enter A Task"
+                      value={input}
+                      required
+                      onChange={onInputChange}
+                    />
+                  </div>
+                  <div className="col-md-2">
+                    <button className="btn submit" type="submit">
+                      {editTodo ? "OK" : "ADD"}
+                    </button>
+                  </div>
+                </div>
+              </form>
+              <TodoList todos={todos} setTodos={setTodos} setEditTodo={setEditTodo} />
+            </div>
           </div>
-          <div className="col-md-2">
-            <button className="btn submit" type="submit">
-              {editTodo ? "OK" : "ADD"}
-            </button>
-          </div>
+
         </div>
-      </form>
+
+      </div >
+
     </div>
   );
 };
